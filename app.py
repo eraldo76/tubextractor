31949a2a65b189eee6a9e1e5155965d99fa684c9
eraldo76 <eraldolattari@gmail.com>
2023-07-15 23:29:26 +0200

    prova senza profix

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
 from settings import YOUTUBE_API_KEY, SECRET_KEY
-from werkzeug.middleware.proxy_fix import ProxyFix  # import ProxyFix
+# from werkzeug.middleware.proxy_fix import ProxyFix  # import ProxyFix
 import requests
 import logging
 import re
@@ -22,7 +22,7 @@ from flask_cdn import CDN
 app = Flask(__name__)
 
 # Add the ProxyFix middleware
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)  # add this line
+# app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1)  # add this line
 
 # Initialize Flask-Minify
 minify(app=app)
